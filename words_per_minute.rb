aded7484701aabd3df8ae47ce960d18ec453db3a
captconrado <ottey001@gmail.com>
2013-03-25 21:08:33 -0700

    added different contexts

--- words_per_minute.rb
+++ words_per_minute.rb
@@ -3,9 +3,12 @@
 
 file = File.read('mooc.txt')
 total_words = file.split.count.to_i
-avg_wpm = 300
+avg_wpm_prose = 300
+avg_wpm_proof = 200
+avg_wpm_scren = 180
 your_avg_wpm = 0
 
+puts "Please input the time it took you to read the text."
 print "Minutes: ";minutes = gets.chomp()
 print "Seconds: ";seconds = gets.chomp()
 
@@ -20,21 +23,28 @@ end
 
 your_avg_wpm = wpm_calc(minutes,seconds,total_words)
 
-def compare(your_avg_wpm,avg_wpm)
-	you = your_avg_wpm.to_f
-	them = avg_wpm.to_f
-
-	k = you/them
-	k = k*100
+def compare(current_wpm,nat_wpm)
+	you = current_wpm.to_f
+	them = nat_wpm.to_f
+	k = you/them*100
 	k = k.to_i
 	return k
 end
 
-your_perc = compare(your_avg_wpm,avg_wpm)
 
+prose_per = compare(your_avg_wpm,avg_wpm_prose)
+proof_per = compare(your_avg_wpm,avg_wpm_proof)
+scren_per = compare(your_avg_wpm,avg_wpm_scren)
 
-puts "Your average words per minute should be #{avg_wpm}"
+perc = [[prose_per,'reading Prose'],[proof_per,'Proof Reading'],[scren_per,'reading of a Monitor']]
+
+puts "Average wpm speed is Prose:#{avg_wpm_prose} Proof Reading:#{avg_wpm_proof} and Screen:#{avg_wpm_scren}"
 puts "You have a current WPM average of #{your_avg_wpm}"
-puts "This is #{your_perc} of the nat average"
+
+perc.each do |x|
+	puts "If you are #{x.last} you are reading at #{x.first}% of National average."
+end
+
+# if your_avg_wpm 
 
 
